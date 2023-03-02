d8b22b8afa0daa216ec293d98c7f0fca39679a0d
ibrahim-lashari <524Ilashari@frhsd.com>
2023-03-02 11:56:41 -0500

    Made computer selection work,  have to do score

--- script.js
+++ script.js
@@ -2,24 +2,30 @@
 document.getElementById('rock').className = 'choice';
 document.getElementById('paper').className = 'choice';
 document.getElementById('scissors').className = 'choice';
+document.getElementById('cpuRock').style.display = 'none';
+document.getElementById('cpuPaper').style.display = 'none';
+document.getElementById('cpuScissors').style.display = 'none';
 
     function hidePanels(){     // Hides panels that are not the active one //
 		document.getElementById('rock').style.display = 'none';
 		document.getElementById('paper').style.display = 'none';
 		document.getElementById('scissors').style.display = 'none';
+        document.getElementById('cpuRock').style.display = 'none';
+		document.getElementById('cpuPaper').style.display = 'none';
+		document.getElementById('cpuScissors').style.display = 'none';
 	}
 
     function getComputerChoice(){ // Computer will make a randomized choice after the player chooses //
         let randomNumber = Math.floor(Math.random() * 3)
         switch (randomNumber) {
             case 0:
-                return cpuRock();
+                return computerRock();
                 break;
             case 1:
-                return cpuPaper();
+                return computerPaper();
                 break;
             case 2:
-                return cpuScissors();
+                return computerScissors();
                 break;
             }
     }
@@ -27,19 +33,20 @@ document.getElementById('scissors').className = 'choice';
 
 
 
-function cpuRock(){ // When person chooses, computer picks rock //
-    document.getElementById('computer').style.display = '';
+function computerRock(){ // When person chooses, computer randomly picks rock //
+    document.getElementById('cpuRock').style.display = 'block';
+    document.getElementById('cpuRock').className = 'afterChoice';
 }
 
-function cpuPaper(){ // When person chooses, computer picks paper //
-    document.getElementById('computer').style.display = '';
+function computerPaper(){ // When person chooses, computer randomly picks paper //
+    document.getElementById('cpuPaper').style.display = 'block';
+    document.getElementById('cpuPaper').className = 'afterChoice';
 }
 
-function cpuScissors() { // When person chooses, computer picks scissors //
-    document.getElementById('computer').style.display = '';
+function computerScissors() { // When person chooses, computer randomly picks scissors //
+    document.getElementById('cpuScissors').style.display = 'block';
+    document.getElementById('cpuScissors').className = 'afterChoice';
 }
-
-
 
 
 function playRock (){ // When rock is clicked, person will play rock //
@@ -54,9 +61,11 @@ function playScissors (){ // When scissors is clicked, person will play scissors
     hidePanels();
     document.getElementById('scissors').style.display = 'block';
 }
-function reset (){
+function reset (){ // Resets the game //
     document.getElementById('rock').style.display = '';
 	document.getElementById('paper').style.display = '';
 	document.getElementById('scissors').style.display = '';
-    document.getElementById('computer').style.display = '';
+    document.getElementById('cpuRock').style.display = 'none';
+    document.getElementById('cpuPaper').style.display = 'none';
+    document.getElementById('cpuScissors').style.display = 'none';
 }
