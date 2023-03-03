1c5040a5c04b8f90a62ef2ef6368fda7a2007cbf
ibrahim-lashari <524Ilashari@frhsd.com>
2023-03-03 08:20:23 -0500

    yt

--- script.js
+++ script.js
@@ -31,35 +31,42 @@ document.getElementById('cpuScissors').style.display = 'none';
     }
 
 
-
+let computerChoice = ""
 
 function computerRock(){ // When person chooses, computer randomly picks rock //
     document.getElementById('cpuRock').style.display = 'block';
     document.getElementById('cpuRock').className = 'afterChoice';
+    let computerChoice = "rock"
 }
 
 function computerPaper(){ // When person chooses, computer randomly picks paper //
     document.getElementById('cpuPaper').style.display = 'block';
     document.getElementById('cpuPaper').className = 'afterChoice';
+    let computerChoice = "paper"
 }
 
 function computerScissors() { // When person chooses, computer randomly picks scissors //
     document.getElementById('cpuScissors').style.display = 'block';
     document.getElementById('cpuScissors').className = 'afterChoice';
+    let computerChoice = "scissors"
 }
 
+let userChoice = ""
 
 function playRock (){ // When rock is clicked, person will play rock //
     hidePanels();
     document.getElementById('rock').style.display = 'block';
+    let userChoice = "rock"
 }
 function playPaper (){ // When paper is clicked, person will play paper //
     hidePanels();
     document.getElementById('paper').style.display = 'block';
+    let userChoice = "paper"
 }
 function playScissors (){ // When scissors is clicked, person will play scissors //
     hidePanels();
     document.getElementById('scissors').style.display = 'block';
+    let userChoice = "scissors"
 }
 function reset (){ // Resets the game //
     document.getElementById('rock').style.display = '';
@@ -68,4 +75,35 @@ function reset (){ // Resets the game //
     document.getElementById('cpuRock').style.display = 'none';
     document.getElementById('cpuPaper').style.display = 'none';
     document.getElementById('cpuScissors').style.display = 'none';
+}
+
+let playerScore = 0
+let computerScore = 0
+
+if (userChoice = "rock" , computerChoice = "rock") {
+
+} 
+else if (userChoice = "rock" , computerChoice = "paper"){
+
+} 
+else if (userChoice = "rock" , computerChoice = "scissors"){
+
+}
+else if (userChoice = "paper" , computerChoice = "rock"){
+
+}
+else if (userChoice = "paper" , computerChoice = "paper"){
+
+}
+else if (userChoice = "paper" , computerChoice = "scissors"){
+
+}
+else if (userChoice = "scissors" , computerChoice = "rock"){
+
+}
+else if (userChoice = "scissors" , computerChoice = "paper"){
+
+}
+else if (userChoice = "scissors" , computerChoice = "scissors"){
+
 }
